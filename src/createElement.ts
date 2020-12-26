92605ca820ee402cf4776027612d6a08fbf18aa1
Johannes Stein <johannesstein@freeze-dev.com>
2020-12-26 18:28:38 +0000

    Remove unneeded symbol

--- src/createElement.ts
+++ src/createElement.ts
@@ -1,5 +1,3 @@
-import create from '.';
-
 const createElement = (width: number, height: number): HTMLCanvasElement => {
   const canvasElement = document.createElement('canvas');
   canvasElement.width = width;
