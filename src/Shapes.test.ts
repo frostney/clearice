9be5447c37524672cc6f2f4fc10d10ef9642f358
Johannes Stein <johannesstein@freeze-dev.com>
2020-12-26 18:24:02 +0000

    Add more tests

--- src/Shapes.test.ts
+++ src/Shapes.test.ts
@@ -1,4 +1,4 @@
-import { Rect } from './Shapes';
+import { Rect, Circle, Point } from './Shapes';
 import { InstructionType } from './types';
 
 describe('Given a Rect function', () => {
@@ -10,6 +10,36 @@ describe('Given a Rect function', () => {
         y: 50,
         w: 100,
         h: 100,
+        color: 'red',
+      },
+    });
+  });
+});
+
+describe('Given a Circle function', () => {
+  it('should return an instruction', () => {
+    expect(
+      Circle({ x: 50, y: 50, w: 100, h: 100, color: 'red' })
+    ).toStrictEqual({
+      type: InstructionType.DRAW_CIRCLE,
+      data: {
+        x: 50,
+        y: 50,
+        w: 100,
+        h: 100,
+        color: 'red',
+      },
+    });
+  });
+});
+
+describe('Given a Point function', () => {
+  it('should return an instruction', () => {
+    expect(Point({ x: 50, y: 50, color: 'red' })).toStrictEqual({
+      type: InstructionType.DRAW_POINT,
+      data: {
+        x: 50,
+        y: 50,
         color: 'red',
       },
     });
